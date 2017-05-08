8d9fb51b410f7738661e48d8db7d518e7dc04e7d
Henrique <henriquepjv@hotmail.com>
2017-05-08 00:29:07 -0300

    Feature: Visitor can send contact email
    - tests for mailer
    - remove sendgrid key
    - simpleform gem
    - create contact model
    - add dotenv gem
    - create dotenv test, dev, production files

---
+++ spec/features/visitor_contact_me_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'visit contact page' do
+  scenario 'Successfully' do
+    visit root_path
+
+    click_on 'Contato'
+    expect(page).to have_content 'Preencha com o seu email:'
+    expect(page).to have_content 'Assunto'
+    expect(page).to have_content 'Mensagem'
+  end
+
+  scenario 'and send contact message' do
+    expect(FormMailer).to receive(:contact_msg).once.and_call_original
+
+    contact = create(:contact)
+
+    visit new_contact_path
+
+    fill_in 'Preencha com o seu email:', with: contact.email
+    fill_in 'Assunto', with: contact.subject
+    fill_in 'Mensagem', with: contact.body
+
+    click_on 'Enviar'
+
+  end
+
+  scenario 'and should fill all fields' do
+
+    visit new_contact_path
+
+    click_on 'Enviar'
+
+    expect(current_path).to eq contacts_path
+    expect(page).to have_content 'Preencha todos os campos'
+
+  end
+end
